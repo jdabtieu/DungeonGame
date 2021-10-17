e28515d586dd7a47f7d5e80b3044ac948d2353a7
Jonathan Wu <jonathan.wu3@outlook.com>
2021-10-17 15:09:07 -0400

    make quiz game more user friendly

--- src/Stage3.java
+++ src/Stage3.java
@@ -300,7 +300,7 @@ public class Stage3 {
     private static void level5Trivia(String ques, String ans) {
         System.out.println(ques);
         System.out.print(">>> ");
-        if (Main.in.readLine().trim().equalsIgnoreCase(ans)) {
+        if (Main.in.readLine().trim().replace(" ", "").equalsIgnoreCase(ans)) {
             System.out.println("That is correct!!!!!!");
             Util.safeSleep(1000);
             System.out.println("You won: 1200 coins");
